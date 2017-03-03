197105d3371c08cecb7b31e003c74f1886b5e4fa
Dias Alves <thomas.dias-alves@imag.fr>
2017-03-03 11:18:41 +0100

    url

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name="aede",
       version='0.1',
       description='Simulation Package in Genomic',
       author='Thomas Dias Alves',
+      url='https://github.com/BioShock38/aede'
       license="MIT",
       packages=find_packages(),
       zip_sage=False)
